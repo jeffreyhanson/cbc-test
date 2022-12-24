1283b852b785fd3da0788c18e3e654f4734a472f
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2022-12-24 16:01:37 +1300

    try again

--- .github/workflows/bundle.sh
+++ .github/workflows/bundle.sh
@@ -11,8 +11,8 @@ URLS=$(pacman -Sp $pkgs --cache=$OUTPUT)
 VERSION=$(pacman -Si mingw-w64-x86_64-${PACKAGE} | awk '/^Version/{print $3}')
 
 # Set version for next step
-echo "::set-output name=VERSION::${VERSION}"
-echo "::set-output name=PACKAGE::${PACKAGE}"
+echo "{VERSION}=${VERSION}" >> $GITHUB_OUTPUT
+echo "{PACKAGE}=${PACKAGE}" >> $GITHUB_OUTPUT
 echo "Bundling $PACKAGE-$VERSION"
 echo "# $PACKAGE $VERSION" > README.md
 echo "" >> README.md
@@ -29,19 +29,8 @@ done
 # Put into dir structure
 rm -Rf lib lib-8.3.0 lib-4.9.3 include share
 mkdir -p lib lib-8.3.0 include share
-
-echo "here1"
-ls -la ${OUTPUT}/mingw64
-echo "here2"
-ls -la ${OUTPUT}/mingw64/include
-echo "here3"
-ls -la ${OUTPUT}/mingw64/lib
-echo "here4"
-ls -la ${OUTPUT}/mingw64/share
-echo "here5"
-
 cp -Rf ${OUTPUT}/mingw64/include include/
-cp -Rf ${OUTPUT}/mingw64/share/cbc share/
+cp -Rf ${OUTPUT}/mingw64/share/ share/
 cp -Rf ${OUTPUT}/mingw64/lib lib-8.3.0/x64
 cp -Rf ${OUTPUT}/mingw32/lib lib-8.3.0/i386
 cp -Rf ${OUTPUT}/ucrt64/lib lib/x64
